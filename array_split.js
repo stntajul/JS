f33eb5939da2a8d84011f8b7902a2dfc4a3b8c1c
tajul islam <tajul.stn99@gmail.com>
2021-09-23 16:21:45 +0600

    array split added

---
+++ array_split.js
@@ -0,0 +1,9 @@
+const nums=[1,2,3,4,5,6,7,8,9,10,12,13,14,15,156];
+//slice
+const another=nums.slice(2,5);
+console.log(another);
+const removed=nums.splice(2,4);
+console.log(nums);
+console.log(removed);
+const together=nums.join(':  ja iccha add kora jay ');
+console.log(together);
